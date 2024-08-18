085703d3f4ebe89d7fa45c29fbd446d32f3ad36b
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2024-08-18 20:36:26 +0530

    Update log_out.py

--- authentication/log_out.py
+++ authentication/log_out.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+print("from login page")
 
 print("from lofout page="+str(st.session_state["authentication_status"]))
 if st.session_state.authenticator.logout("Logout", "main"):
     st.session_state.priviledge = False
     st.rerun()
+
+# Creating an update user details widget
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.update_user_details(st.session_state["username"]):
+            st.success('Entries updated successfully')
+    except UpdateError as e:
+        st.error(e)
+
+# Saving config file
+with open('config.yaml', 'w', encoding='utf-8') as file:
+    yaml.dump(config, file, default_flow_style=False)
